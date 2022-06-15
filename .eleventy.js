66df24e9ced2bd1dec97565c634e6c789888f2ce
JonS7 <jon@satellite7.com>
2022-06-15 15:16:06 +0100

    gallery shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -1,81 +1,113 @@
 const Image = require("@11ty/eleventy-img");
 const glob = require("glob-promise");
-const path = require("path");
+const htmlmin = require("html-minifier");
 
 const THUMB = 400;
 const LARGE = 1000;
 const FULL = 1500;
 
-async function generateImages() {
+function generateCSSImages(src, widths){
+  let options = {
+    widths: widths,
+    formats: ['jpeg',],
+    outputDir: "./public/img/",
+  };
+  // genrate images, ! dont wait
+  Image(src, options);
+  // get metadata even the image are not fully generated
+  return Image.statsSync(src, options);
+}
 
-  const defaultOpts = {
-    widths: [THUMB,LARGE,FULL],
+function imageCssBackground (src, selector, widths){
+  const metadata = generateCSSImages(src, widths);
+  let markup = [`${selector} { background-image: url(${metadata.jpeg[0].url});} `];
+  // i use always jpeg for backgrounds
+  metadata.jpeg.slice(1).forEach((image, idx) => {
+    markup.push(`@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`);
+  });
+  return markup.join("");
+}
+
+
+function generateGallery(src, alt) {
+  let options = {
+    widths: [THUMB,LARGE],
     formats: [null],
-    filenameFormat: function (id, imgSrc, width, format, options) {
-      let origFilename = imgSrc.split('/').pop();
-      //strip off the file type, this could probably be one line of fancier JS
-      let parts = origFilename.split('.');
-      parts.pop();
-      origFilename = parts.join('.');
-
-      if(width === THUMB) return `thumb-${origFilename}.${format}`;
-      else return `${origFilename}-w${width}.${format}`;
-    }
+    outputDir: './public/img/gallery/',
+    urlPath: '/img/gallery/',
   };
-  const IMG_SRC = './src/images/';
-
-  let files = await glob('./src/images/**/*.*');
-  for(const src of files) {
-    console.log('doing src',src);
-
-    let outputDir = './img/';
-    let urlPath = '/img/';
-
-    const dir = path.dirname(src.slice(IMG_SRC.length));
-    outputDir = `${outputDir}${dir}`;
-    urlPath = `${urlPath}${dir}`;
-   
-    const opts = {
-      ...defaultOpts,
-      outputDir,
-      urlPath,
-    };
-
-    let md = await Image(src, opts);
-  };
+  // genrate images, ! dont wait
+  Image(src, options);
+  // get metadata even the image are not fully generated
+  return Image.statsSync(src, options);
 };
 
-module.exports = function (config) {
+async function galleryShortcode(src, alt, size) {
+  const metadata = generateGallery(src, alt);
+  let sizeSrc = metadata["jpeg"][0];
+  if(size == "full") {
+     sizeSrc = metadata["jpeg"][1];
+  }
+  return `<img src="${sizeSrc.url}" width="${sizeSrc.width}" height="${sizeSrc.height}" alt="${alt}" >`;
+}
 
-  config.addCollection('gallery', async collectionApi => {
 
-    let files = await glob('./img/gallery/*.{jpg,jpeg,png}');
-    //Now filter to 1000px width versions
-    let images = files.filter(f => {
-      return f.indexOf('-w1000') !== -1;
-    });
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [THUMB,LARGE,FULL, null],
+    formats: ['webp', 'jpeg'],
+    outputDir: "./public/img/",
+  });
 
-    let collection = images.map(i => {
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+
+module.exports = function (eleventyConfig) {
+
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode("galleryImage", galleryShortcode);
+  eleventyConfig.addNunjucksShortcode("cssBackground", imageCssBackground);
+
+  eleventyConfig.addPassthroughCopy('src/fonts');
+
+  eleventyConfig.addWatchTarget("./src/styles/");
+
+  eleventyConfig.addCollection('gallery', async collectionApi => {
+    let files = await glob('./src/images/gallery/*');
+    let collection = files.map(i => {
       return {
-        w1000path: i,
-        thumbpath: i.replace('-w1000', '').replace('./img/gallery/', './img/gallery/thumb-')
+        thumbpath: i,
       }
     });
-
     return collection;
-
   });
 
-  config.on('beforeBuild', async () => {
-    console.log('beforeBuild');
-    await generateImages();
-    console.log('images done');
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if( outputPath && outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      });
+      return minified;
+    }
+
+    return content;
   });
 
-  config.addPassthroughCopy('src/fonts');
-  config.addPassthroughCopy('img');
-
-  config.addWatchTarget("./src/styles/");
 
   return {
     dataTemplateEngine: 'njk',
